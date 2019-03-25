6a847abcb996270a6f07b643a8eb02931142ad16
Rinaldo Santana <rinaldo.rms@gmail.com>
2019-03-25 01:59:53 -0300

    Add estrutura de controle e condicional.

---
+++ do-while.dart
@@ -0,0 +1,9 @@
+main() {
+  num i = 0;
+  do {
+    print(i);
+    i++;
+  } while ( i <= 0);
+}
+
+// 0
--- html/index.dart
+++ html/index.dart
@@ -1,11 +1,13 @@
 import 'dart:html';
 
 main() {
+  var app = querySelector('#app');
 
   var botao = new ButtonElement();
   botao..id = 'confirmar'
        ..text = 'Clique para confirmar'
        ..classes.add('importante')
        ..addEventListener('click', (e) => window.alert('confirmado!'));
-  querySelector('#registration').append(botao);
+
+  app.append(botao);
 }
